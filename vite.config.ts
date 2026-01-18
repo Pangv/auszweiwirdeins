26aa4b60e230c975f078598b242aab8073eae0a8
Pangv <stvbiffer@gmail.com>
2026-01-18 17:58:43 +0100

    added cms capability

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
-  base: 'https://pangv.github.io/auszweiwirdeins',
+  base: '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
