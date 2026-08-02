02aebef914a441584450ce597d37a7f2dc583546
Pangv <stvbiffer@gmail.com>
2026-08-02 16:40:52 +0200

    typo

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <footer class="bg-primary py-12 px-5 flex flex-col gap-8 md:flex-row md:justify-between md:items-center text-left">
     <h2 class="text-[clamp(2.5rem,10vw,6rem)] text-secondary font-extrabold leading-none">
-      <slot>Danke schön wars gewesen!</slot>
+      <slot>Danke, schön wars gewesen!</slot>
     </h2>
 
     <div class="flex flex-col gap-2 md:gap-6 md:flex-row md:items-center">
